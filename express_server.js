1c0a4b0248d7d77508c9ec5c9cc439eb1479270d
Moon Choi <choimy1991@gmail.com>
2022-04-19 13:40:42 -0700

    commit

--- express_server.js
+++ express_server.js
@@ -5,10 +5,12 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+//THERE IS NO BODY IN GET REQUEST
 
+//BODY IS JUST FOR THE POST REQUEST.
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com", )
+  "9sm5xK": "http://www.google.com",
 };
 
 const generateRandomString = function () {
@@ -22,7 +24,7 @@ app.get("/", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render("urls_index", templateVars); //brining the files in partials
 });
 
 app.get("/urls.json", (req, res) => {
@@ -33,20 +35,30 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//in urls_new.ejs
+//actions: /urls
 app.post("/urls", (req, res) => {
-  const longURL = req.body.longURL; 
+  const longURL = req.body.longURL; //body inside the post request, pull the 'longURL' info.
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = longURL;
-  res.redirect("/urls"); 
+  res.redirect("/urls/");
 });
 
-app.get("/urls/:id", (req, res) => {
-  const shortURL = req.params.id; 
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
   const templateVars = {
     shortURL: shortURL,
     longURL: urlDatabase[shortURL],
   };
   res.render("urls_show", templateVars);
+});
+
+// body is used for post.
+// get is used for the urls_show.ejs
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
