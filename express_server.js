5b36ca11ab2d9cb5232fba3a968d3859f49e13ac
Moon Choi <choimy1991@gmail.com>
2022-04-19 21:27:21 -0700

    Display the Username

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 //THERE IS NO BODY IN GET REQUEST
 
@@ -17,6 +19,8 @@ const generateRandomString = function () {
   const str = Math.random().toString(36).slice(7);
   return str;
 };
+
+//================GET====================///
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -35,15 +39,6 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-//in urls_new.ejs
-//actions: /urls
-app.post("/urls", (req, res) => {
-  const longURL = req.body.longURL; //body inside the post request, pull the 'longURL' info.
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = longURL;
-  res.redirect("/urls/");
-});
-
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const templateVars = {
@@ -60,6 +55,59 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
+
+//================POST====================///
+
+//in urls_new.ejs
+//actions: /urls
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL; //body inside the post request, pull the 'longURL' info.
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  const templateVars = {
+    //we need to pass it to 69.
+    shortURL: shortURL,
+    longURL: urlDatabase[shortURL], //saving it to the DB.
+  };
+  res.render("urls_show", templateVars); //after mathching keys are found in the .ejs file, then it shows the values. (Rendering)
+  //rendering means getting the page displayed with the values.
+  // alligator: interpretes dynamic values.
+});
+
+app.post("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//wildcard is :shortURL
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls/");
+});
+
+app.post("/urls/:shortURL/edit", (req, res) => {
+  const newURL = req.body.newURL;
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = newURL;
+  res.redirect("/urls/");
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username; //got from the form
+  res.cookie("cookie", username); //sets the cookie always singular .. setting only one cookie at a time.
+  // console.log("getcookie", req.cookies["cookie"]);
+  const templateVars = {
+    nameEJS: req.cookies["cookie"], //gets all the cookies
+    urls: urlDatabase,
+  };
+  res.render("urls_index", templateVars);
+});
+
+// app.post("/logout", (req, res) => {
+//   const username = req.body.username; //got from the form
+//   res.cookie("cookie", username);
+//   res.redirect("/urls/");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
